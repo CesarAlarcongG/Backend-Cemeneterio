2986322f930248a047b749ced620fb93b561536b
Cesar <cesarjoelalarconguzman@gmail.com>
2024-09-28 12:09:15 -0500

    agregando archivos al proyecto

--- src/main/java/com/Backend/BackendCementerio/usuario/persistence/repositoy/IUsuarioRepository.java
+++ src/main/java/com/Backend/BackendCementerio/usuario/persistence/repositoy/IUsuarioRepository.java
@@ -1,7 +1,6 @@
 package com.Backend.BackendCementerio.usuario.persistence.repositoy;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.Backend.BackendCementerio.usuario.persistence.model.Usuario;
