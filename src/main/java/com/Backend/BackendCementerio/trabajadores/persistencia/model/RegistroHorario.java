a79b0a5edd35889dfb9502ea1636abd2901a8c11
Cesar Alarcon <cesarjoelalarconguzman@gmail.com>
2024-10-27 18:59:16 -0500

    "creamos nuevos endpoint que tiene la funcionalidad de 1. Regresa una lista de empleados 2. crear nuevos empleados"

--- src/main/java/com/Backend/BackendCementerio/trabajadores/persistencia/model/RegistroHorario.java
+++ src/main/java/com/Backend/BackendCementerio/trabajadores/persistencia/model/RegistroHorario.java
@@ -1,7 +1,6 @@
 package com.Backend.BackendCementerio.trabajadores.persistencia.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/com/Backend/BackendCementerio/trabajadores/persistencia/repository/TrabajadorRespository.java
+++ src/main/java/com/Backend/BackendCementerio/trabajadores/persistencia/repository/TrabajadorRespository.java
@@ -4,7 +4,6 @@ package com.Backend.BackendCementerio.trabajadores.persistencia.repository;
 import com.Backend.BackendCementerio.trabajadores.persistencia.model.Trabajador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface TrabajadorRespository extends JpaRepository<Trabajador, Long> {
