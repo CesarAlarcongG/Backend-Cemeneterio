93b4536d9247c4ed43bf9136e7175e690edfc3cc
Cesar <cesarjoelalarconguzman@gmail.com>
2024-10-28 20:32:14 -0500

    "Agregando cors"

---
+++ src/main/java/com/Backend/BackendCementerio/usuario/security/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.Backend.BackendCementerio.usuario.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuración de peticiones
+ * Este archivo fue creado con la intención de poder configurar la seguridad de recepción de métodos.
+ * A modo de prueba hemos consideado recibir cualquier petición desde cualquier servidor.
+ */
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry){
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*");
+
+    }
+    
+}
